eab4301f5da6bc001e8fda7c8587f83556d93dff
Nati Mal <natanaelmalita@gmail.com>
2023-04-18 17:45:40 +0300

    seo

--- pages/reparo.js
+++ pages/reparo.js
@@ -5,10 +5,10 @@ export default function Reparo() {
   return (
     <><Head>
       <title>Reparo (Atelierul Art Cafe)</title>
-        <meta name="description" content="Estimare cost reparație gratuită pentru aparate de cafea. Servicii rapide și eficiente pentru o cafea perfectă în fiecare dimineață!
+        <meta name="description" content="Estimare cost gratuită pentru reparații aparate de cafea. Servicii rapide și eficiente pentru o cafea perfectă în fiecare dimineață!
 Revitalizăm aparatul tău de cafea automat, pentru o aromă și o funcționare perfectă!" />
       <meta property="og:title" content="Reparo (Atelierul Art Cafe)" />
-    <meta property="og:description" content="Estimare cost reparație gratuită pentru aparate de cafea. Servicii rapide și eficiente pentru o cafea perfectă în fiecare dimineață!
+    <meta property="og:description" content="Estimare cost gratuită pentru reparații aparate de cafea. Servicii rapide și eficiente pentru o cafea perfectă în fiecare dimineață!
 Revitalizăm aparatul tău de cafea automat, pentru o aromă și o funcționare perfectă!"  />
     <meta property="og:image" itemprop="image" content={`${process.env.NEXT_PUBLIC_URL}/assets/estimate/cleaning.png`}/>
     <meta property="og:updated_time" content="1681823297"/></Head>
